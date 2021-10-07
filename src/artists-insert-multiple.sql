f221982d74257648b2d2c68188a98d72b4dda948
davecate <86169488+davecate@users.noreply.github.com>
2021-10-07 12:10:52 -0500

    All scripts completed, passing all tests.

---
+++ src/artists-insert-multiple.sql
@@ -0,0 +1,27 @@
+insert into artists
+  (artist_name, birthday, birthplace, is_alive)
+  
+  values (
+    'this name',
+    '1234-01-23',
+    'this birthplace',
+    'false'
+  ),
+  (
+    'that name',
+    '1234-01-23',
+    'that birthplace',
+    'true'
+  ),
+  (
+    'the other name',
+    '1234-01-23',
+    'the other birthplace',
+    'true'
+  ),
+  (
+    'yet another name',
+    '1234-01-23',
+    'yet another birthplace',
+    'true'
+  )
